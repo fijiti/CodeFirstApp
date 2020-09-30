d0383aee67ccb859f3065d8a4c752fc3e8068a2c
fijiti <fijiti@users.noreply.github.com>
2020-09-29 22:00:00 -0700

    Add project files.

---
+++ CodeFirstApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using (var ctx = new SchoolContext())
+            {
+                var student = new Student
+                {
+                    Id = 1,
+                    FirstName = "Joe",
+                    LastName = "Smith",
+                };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+                foreach(var aStudent in ctx.Students)
+                {
+                    Console.WriteLine($"Student {aStudent.FirstName} {aStudent.LastName} Id is {aStudent.Id}.");
+                }
+               
+                Console.ReadLine();
+            } 
+        }
+    }
+}
---
+++ CodeFirstApp/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace CodeFirstApp
+{
+    public class SchoolContext: DbContext
+    {
+
+        public SchoolContext(): base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
